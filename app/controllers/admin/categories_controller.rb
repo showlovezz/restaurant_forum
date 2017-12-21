82ac5a0f99eb8a1573eb289fe8d42b8ed344c31b
vita <dragic.hsieh@gmail.com>
2017-12-21 18:09:31 +0800

    use can delete category with link on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -32,7 +32,14 @@ class Admin::CategoriesController < ApplicationController
 			@categories = Category.all
 			render :index
 		end
-	end			
+	end
+
+	def destroy
+		@category = Category.find(params[:id])
+		@category.destroy
+		flash[:alert] = "category was successfully deleted"
+		redirect_to admin_categories_path
+	end				
 
 	private
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, PhotoUploader
 	validates_presence_of :name
 	belongs_to :category
+	delegate :name, to: :category, prefix: true, allow_nil: true
 end
