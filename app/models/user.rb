55ff48b371b86d57e3a6f968d36915f789531c0a
vita <dragic.hsieh@gmail.com>
2018-01-04 16:02:12 +0800

    setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,11 @@ class User < ApplicationRecord
   
   # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
   has_many :comments, dependent: :restrict_with_error
-
   has_many :restaurants, through: :comments
+
+  # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
   def admin?
   	self.role == "admin"
