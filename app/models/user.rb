91da93145296be4256a6ccf780e0cba4189c9efd
vita <dragic.hsieh@gmail.com>
2018-01-06 13:36:03 +0800

    add like model and setup relation among user, restaurant and like

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
+  # 「使用者喜歡很多餐廳」的多對多關聯
+  has_many :likes, dependent: :destroy
+  has_many :liked_restaurants, through: :likes, source: :restaurant
+
   def admin?
   	self.role == "admin"
   end	       
